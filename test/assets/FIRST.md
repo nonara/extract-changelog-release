f4ef3d2913ad98d08f73f406f0a81a46d8da5992
Ron S <ron@nonara.com>
2021-08-16 17:45:52 -0400

    fix-: Support non-linked headers

---
+++ test/assets/FIRST.md
@@ -0,0 +1,10 @@
+# Changelog
+
+All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
+
+## 1.0.0 (2021-08-16)
+
+
+### Features
+
+* First version complete ([03026b4](https://github.com/nonara/extract-changelog-release/commit/03026b432806f306259b186a7493a5e57c895198))
---
+++ test/specific.test.ts
@@ -0,0 +1,17 @@
+import { extractLog } from '../src';
+import path from 'path';
+
+
+/* ****************************************************************************************************************** *
+ * Tests
+ * ****************************************************************************************************************** */
+
+describe(`Specific Tess`, () => {
+  test(`Supports headings without link`, () => {
+    expect(extractLog(path.resolve(__dirname, 'assets/FIRST.md'))).toBe(
+      `## 1.0.0 (2021-08-16)\n\n` +
+      `### Features\n\n` +
+      `* First version complete ([03026b4](https://github.com/nonara/extract-changelog-release/commit/03026b432806f306259b186a7493a5e57c895198))`
+    );
+  });
+});
