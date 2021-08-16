92bea500021565d5e7451e2543aaf7981a2d1069
Ron S <ron@nonara.com>
2021-08-16 17:50:14 -0400

    refactor: Accommodate for linefeed differences in new test

--- test/specific.test.ts
+++ test/specific.test.ts
@@ -8,7 +8,7 @@ import path from 'path';
 
 describe(`Specific Tess`, () => {
   test(`Supports headings without link`, () => {
-    expect(extractLog(path.resolve(__dirname, 'assets/FIRST.md'))).toBe(
+    expect(extractLog(path.resolve(__dirname, 'assets/FIRST.md')).replace(/\r?\n/g, '\n')).toBe(
       `## 1.0.0 (2021-08-16)\n\n` +
       `### Features\n\n` +
       `* First version complete ([03026b4](https://github.com/nonara/extract-changelog-release/commit/03026b432806f306259b186a7493a5e57c895198))`
